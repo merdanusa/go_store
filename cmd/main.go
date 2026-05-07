ec6c3b7b6dff203f617cd9cd4c0468d1d9793aa6
Merdan <wisew.tube@gmail.com>
2026-05-07 17:25:02 +0500

    Init go backend setup on Fiber

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber/v3"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c fiber.Ctx) error {
+		return c.SendString("yo it works")
+	})
+
+	app.Listen(":3000")
+}
